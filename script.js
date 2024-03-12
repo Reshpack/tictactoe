8eb244cc5d862debd859b8901a34c5c4d51def9d
Suresh Packiry <sureshpackiry73@gmail.com>
2024-03-13 10:36:04 +1100

    finished the css starting the js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// Menu screen 
 document.addEventListener('keydown', function(event) {
     if (event.code === 'Space') {
         // Hide the start page
@@ -7,3 +9,54 @@ document.addEventListener('keydown', function(event) {
     }
 });
 
+// Game
+const cells = document.querySelector(".cell");
+const statusText = document.querySelector("#statusText");
+const restartButton = document.querySelector("#restartButton");
+const returnMenu = document.querySelector("#returnMenu")
+const winConditions = [
+    [0, 1 ,2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7], 
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+let options = ["", "", "", "", "", "", "", ""];
+let currentPlayer = "X"
+let running = false;
+
+initialiseGame();
+
+function initialiseGame() {
+    cells.forEach(cell => cell.addEventListener("click", cellClicked));
+    restartButton.addEventListener("click", restartGame);
+    statusText.textContent = `${currentPlayer}'s turn`
+};
+
+function cellClicked() {
+    
+};
+
+function updateCell(cell, index) {
+
+};
+
+function changePlayer() {
+
+};
+
+function checkWinner() {
+
+};
+
+function restartGame() {
+
+};
+
+function menuReturn() {
+
+}
